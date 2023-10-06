8a74bf766d1bf41833550598bb4d3578f8bf8fdf
Tobias Klug <tbs.klg@gmail.com>
2023-10-06 16:48:14 +0200

    Change rosepine color mode

--- lua/tbsklg/plugins/color.lua
+++ lua/tbsklg/plugins/color.lua
@@ -2,6 +2,10 @@ return {
 	"rose-pine/neovim",
 	config = function()
 		require("rose-pine").setup({
+			--- @usage 'auto'|'main'|'moon'|'dawn'
+			variant = "moon",
+			--- @usage 'main'|'moon'|'dawn'
+			dark_variant = "main",
 			disable_italics = true,
 		})
 
