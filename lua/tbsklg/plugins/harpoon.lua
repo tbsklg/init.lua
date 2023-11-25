e215bcea679c31015ac97b27f5f164299ff21877
Tobias Klug <tbs.klg@gmail.com>
2023-11-25 07:51:43 +0100

    Update nvim config

--- lua/tbsklg/plugins/harpoon.lua
+++ lua/tbsklg/plugins/harpoon.lua
@@ -4,17 +4,17 @@ return {
 		local mark = require("harpoon.mark")
 		local ui = require("harpoon.ui")
 
-		vim.keymap.set("n", "<leader>a", mark.add_file)
-		vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+		vim.keymap.set("n", "<leader>a", mark.add_file, { desc = "Add file to harpoon" })
+		vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu, { desc = "Toggle harpoon menu" })
 
 		vim.keymap.set("n", "<C-t>", function()
-			ui.nav_file(2)
-		end)
+			ui.nav_file(3)
+		end, { desc = "Navigate to file in harpoon" })
 		vim.keymap.set("n", "<C-n>", function()
 			ui.nav_file(3)
-		end)
+		end, { desc = "Navigate to file in harpoon" })
 		vim.keymap.set("n", "<C-s>", function()
 			ui.nav_file(4)
-		end)
+		end, { desc = "Navigate to file in harpoon" })
 	end,
 }
--- lua/tbsklg/plugins/lsp.lua
+++ lua/tbsklg/plugins/lsp.lua
@@ -81,6 +81,11 @@ return {
 			on_attach = on_attach,
 		})
 
+		lspconfig["rust_analyzer"].setup({
+			capabilities = capabilities,
+			on_attach = on_attach,
+		})
+
 		-- configure lua server (with special settings)
 		lspconfig["lua_ls"].setup({
 			capabilities = capabilities,
