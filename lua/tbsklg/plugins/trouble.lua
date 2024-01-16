a818ad7cf5085d07d3d5313b41bf525d12d75f6c
Tobias Klug <tbs.klg@gmail.com>
2024-01-16 12:22:53 +0100

    Update trouble config

--- lua/tbsklg/plugins/trouble.lua
+++ lua/tbsklg/plugins/trouble.lua
@@ -2,6 +2,8 @@ return {
 	"folke/trouble.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	opts = {
+		position = "bottom",
+		diagnostics = "document_diagnostics",
 		fold_closed = "",
 		fold_open = "",
 		auto_open = true,
