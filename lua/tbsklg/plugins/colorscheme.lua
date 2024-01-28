7b554f51d9d76f0fd46bbe4dffdfbf55ac27a72f
Tobias Klug <tbs.klg@gmail.com>
2024-01-28 19:43:19 +0100

    Update color schema

--- lua/tbsklg/plugins/colorscheme.lua
+++ lua/tbsklg/plugins/colorscheme.lua
@@ -1,9 +1,19 @@
 return {
 	{
-		"bluz71/vim-nightfly-guicolors",
-		priority = 1000, -- make sure to load this before all the other start plugins
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
 		config = function()
-			vim.cmd([[colorscheme nightfly]])
+			local cattppuccin = require("catppuccin")
+
+			cattppuccin.setup({
+				flavour = "mocha",
+				integrations = {
+					neotree = true,
+				},
+			})
+
+			vim.cmd([[colorscheme catppuccin]])
 		end,
 	},
 }
---
+++ lua/tbsklg/plugins/indent-blankline.lua
@@ -0,0 +1,21 @@
+return {
+	"lukas-reineke/indent-blankline.nvim",
+	main = "ibl",
+	opts = {
+		exclude = {
+			filetypes = {
+				"help",
+				"alpha",
+				"dashboard",
+				"neo-tree",
+				"Trouble",
+				"trouble",
+				"lazy",
+				"mason",
+				"notify",
+				"toggleterm",
+				"lazyterm",
+			},
+		},
+	},
+}
