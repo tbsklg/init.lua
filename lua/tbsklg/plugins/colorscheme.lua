30c783b105e921fa84557c0508f18e7169b79d08
Tobias Klug <tbs.klg@gmail.com>
2024-02-12 06:45:11 +0100

    Use rose pine because it is f***ing awesome

--- lua/tbsklg/plugins/colorscheme.lua
+++ lua/tbsklg/plugins/colorscheme.lua
@@ -1,21 +1,3 @@
---return {
---	"catppuccin/nvim",
---	name = "catppuccin",
---	priority = 1000,
---	config = function()
---		local cattppuccin = require("catppuccin")
---
---		cattppuccin.setup({
---			flavour = "mocha",
---			integrations = {
---				neotree = true,
---			},
---		})
---
---		vim.cmd([[colorscheme catppuccin]])
---	end,
---}
-
 return {
 	"rose-pine/neovim",
 	name = "rose-pine",
