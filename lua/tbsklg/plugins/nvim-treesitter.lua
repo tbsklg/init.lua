82b9b76adffeca1164c934b9ff3a3cb2c9338a80
Tobias Klug <tbs.klg@gmail.com>
2024-04-28 07:34:11 +0200

    Install nvim treesitter playground

--- lua/tbsklg/plugins/nvim-treesitter.lua
+++ lua/tbsklg/plugins/nvim-treesitter.lua
@@ -5,6 +5,7 @@ return {
 		build = ":TSUpdate",
 		dependencies = {
 			"nvim-treesitter/nvim-treesitter-textobjects",
+			"nvim-treesitter/playground",
 			"windwp/nvim-ts-autotag",
 		},
 		config = function()
